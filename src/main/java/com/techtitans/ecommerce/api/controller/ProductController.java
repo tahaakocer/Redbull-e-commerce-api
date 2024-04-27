6ef311c3ad350490607d08b4c1535f37c537df49
tahaakocer <taha.kocer317@gmail.com>
2024-04-27 16:05:18 +0300

    product api calisiyor

--- src/main/java/com/techtitans/ecommerce/api/controller/ProductController.java
+++ src/main/java/com/techtitans/ecommerce/api/controller/ProductController.java
@@ -1,19 +1,14 @@
 package com.techtitans.ecommerce.api.controller;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.EntityResponse;
 
-import com.tahaakocer.newDemo.newDemo.business.responses.GetAllCategoriesResponse;
 import com.techtitans.ecommerce.business.abstracts.ProductService;
 import com.techtitans.ecommerce.business.request.AddProductRequest;
 import com.techtitans.ecommerce.business.responses.ProductGetAllResponse;
